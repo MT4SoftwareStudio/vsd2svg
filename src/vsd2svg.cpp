47ac433a1ffa293ec4f0ff7b91caa31851f48348
Steffen Macke <sdteffen@sdteffen.de>
2012-02-19 20:06:03 +0100

    Soft start: identify number of SVG nodes in xhtml file.

---
+++ src/vsd2svg.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	xmlDocPtr doc;
+	xmlXPathContextPtr xpathCtx;
+	xmlXPathObjectPtr xpathObj;
+	int drawingpageno = 0;
+
+	if((3 < argc)||(4 > argc))
+	{
+		cout << "USAGE: vsd2svg visiofile svgfile [drawingpagenumber]" << endl;
+		return 1;
+	}
+
+	xmlInitParser();
+	LIBXML_TEST_VERSION
+	doc = xmlParseFile(argv[1]);
+	if(NULL == doc)
+	{
+		cerr << "ERROR: Unable to parse file " << argv[1] << endl;
+		return 1;
+	}
+
+	xpathCtx = xmlXPathNewContext(doc);
+	if(NULL == xpathCtx)
+	{
+		cerr << "ERROR: Unable to create new XPath context." << endl;
+		xmlFreeDoc(doc);
+		return 1;
+	}	
+	
+	xmlXPathRegisterNs(xpathCtx, (const xmlChar *)"svg", (const xmlChar *)"http://www.w3.org/2000/svg");
+	xmlXPathRegisterNs(xpathCtx, (const xmlChar *)"xhtml", (const xmlChar *)"http://www.w3.org/1999/xhtml");
+	xmlXPathRegisterNs(xpathCtx, (const xmlChar *)"xlink", (const xmlChar *)"http://www.w3.org/1999/xlink");
+
+	xpathObj = xmlXPathEvalExpression((const xmlChar *)argv[2], xpathCtx);
+	if(NULL == xpathObj)
+	{	
+		cerr << "ERROR: Unable to evaluate xpath expression." << endl;
+		return 1;
+	}
+	cout << "Number of SVG nodes: " << ((xpathObj->nodesetval) ? xpathObj->nodesetval->nodeNr : 0) << endl;
+
+	return 0;
+}
+
